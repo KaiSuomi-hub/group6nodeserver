ef792659c5291212e02fc8477ce38c07e8db6348
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-11-04 18:56:07 +0200

    explain the etc/secrets

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ const fs = require("fs");
  * @property {Buffer} cert - The certificate for SSL, read from the file system.
  */
 const sslOptions = {
-    key: fs.readFileSync("./cert/privatekey.pem"),
-    cert: fs.readFileSync("./cert/certificate.pem"),
-  };
-
+    key: fs.readFileSync("/etc/secrets/privatekey.pem"),
+    cert: fs.readFileSync("/etc/secrets/certificate.pem"),
+};
+/* sslOptions point to etc secrets folder where the private key and certificate are stored in Render
+For local development, change the path to the location of the private key and certificate on your machine.
+Use the following code to generate a self-signed certificate for local development:
+openssl req -nodes -new -x509 -keyout privatekey.pem -out certificate.pem -days 365
+*/
 const app = express();
 const server = http.createServer(sslOptions, app);
 // Socket.IO is initialized with the HTTPS server to enable real-time communication:
@@ -26,49 +30,45 @@ const io = socket(server);
 // An empty object rooms is defined to keep track of the rooms and their participants:
 const rooms = {};
 
-
-
 const port = process.env.PORT || 9000;
 // Handling Socket.IO Events:
 // The server listens for various Socket.IO events to manage WebRTC signaling:
 
 // Connection Event: When a client connects, a connection event is fired, and a message "Connected" is logged:
-io.on("connection", socket => {
-    console.log("Connected")
+io.on("connection", (socket) => {
+    console.log("Connected");
     // Join Room Event: The server listens for a join room event, which is triggered when a client wants to join a specific room:
-    socket.on("join room", roomID => {
+    socket.on("join room", (roomID) => {
         console.log("Join room fired", roomID);
         if (rooms[roomID]) {
-            console.log("Push")
+            console.log("Push");
             rooms[roomID].push(socket.id);
         } else {
-            console.log("Create")
+            console.log("Create");
             rooms[roomID] = [socket.id];
         }
-        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        const otherUser = rooms[roomID].find((id) => id !== socket.id);
         if (otherUser) {
-            console.log("Other user fired and user joined fired")
+            console.log("Other user fired and user joined fired");
             socket.emit("other user", otherUser);
             socket.to(otherUser).emit("user joined", socket.id);
         }
     });
     // Offer Event: The server listens for an offer event, which is part of the WebRTC signaling process:
-    socket.on("offer", payload => {
-        console.log("Offer fired", payload)
+    socket.on("offer", (payload) => {
+        console.log("Offer fired", payload);
         io.to(payload.target).emit("offer", payload);
     });
     // Answer Event: The server listens for an answer event, another part of the WebRTC signaling process:
-    socket.on("answer", payload => {
-        console.log("Answer fired", payload)
+    socket.on("answer", (payload) => {
+        console.log("Answer fired", payload);
         io.to(payload.target).emit("answer", payload);
     });
     // ICE Candidate Event: The server listens for an ice-candidate event, which is used to exchange ICE candidates between peers:
-    socket.on("ice-candidate", incoming => {
+    socket.on("ice-candidate", (incoming) => {
         console.log("Ice candidate fired");
         io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
 });
 
-
-
-server.listen(port, () => console.log('server is running on port 9000'));
+server.listen(port, () => console.log("server is running on port 9000"));
